0ee0c7f879ebb2f1087138039da3e511be5690f2
calebtetteh <calebtetteh65@gmail.com>
2025-07-31 18:26:57 +0000

    Ticket Calculator

---
+++ TicketCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static double ticketCalculator(int age)
+    {
+        if (age <= 12 || age >= 65)
+        {
+            return 7.0;
+        }
+        else
+        {
+            return 10;
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your age: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int age))
+        {
+            double ticketPrice = ticketCalculator(age);
+            Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid age.");
+        }
+    }
+}
